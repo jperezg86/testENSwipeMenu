502e90bfa6b3a3c88e3957c73cfef5d9717fcac5
Jose Manuel Perez <jperezg86@gmail.com>
2016-02-10 05:51:39 -0600

    Commit inicial

---
+++ TestEnSwiftSideMenu/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestEnSwiftSideMenu
+//
+//  Created by Jose Manuel Perez on 2/9/16.
+//  Copyright © 2016 Jose Manuel Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
